af83f1ad24f5ff460b6fb42dec3101c96a4f772b
Bui Trong Thanh <buitrongthanh2k5@gmai.com>
2025-11-22 13:42:47 +0700

    push database to app.vue layout

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
     <div>
-        <h1>My Vue App</h1>
+        <!-- <h1>My Vue App</h1> -->
         <p v-if="!data">Loading...</p>
         <div v-else-if="data.length === 0">Khong co san pham</div>
         <!-- <pre v-else>{{ data }}</pre> -->
         <div v-else>
-            <div v-for="product in data" :key="product.id">
-                <h3>{{ product.name }}</h3>
-                <img :src="product.img" :alt="product.name" />
-                <p>Price: {{ product.price }}</p>
-                <p>Quantity: {{ product.quantity }}</p>
-                <hr />
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4" v-for="product in data" :key="product.id">
+                        <h3>{{ product.name }}</h3>
+                        <img :src="product.img" :alt="product.name" />
+                        <p>Price: {{ product.price }}</p>
+                        <p>Quantity: {{ product.quantity }}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+    <center class="filter-page">
+        <button>1</button>
+        <button>1</button>
+        <button>1</button>
+    </center>
 </template>
 
 <script>
@@ -27,8 +35,8 @@ export default {
     async mounted() {
         const page = 1;
         const limit = 5;
-        const url = `home/api/${page}/${limit}`;
-        console.log("1");
+        const url = `/api/${page}/${limit}`;
+        // console.log("1");
         // const res = await fetch(`Dang goi API o day: ${url}`);
         try {
             const res = await fetch(url);
@@ -45,3 +53,24 @@ export default {
     }
 };
 </script>
+<style scoped>
+div.col-md-4 {
+    border: 1px solid #ccc;
+    padding: 10px;
+    margin-bottom: 20px;
+    text-align: center;
+}
+div.col-md-4 img{
+    max-width: 100%;
+    height: auto;
+}
+center.filter-page {
+    margin-top: 20px;
+    margin-bottom: 100px;
+}
+center.filter-page button {
+    margin: 0 5px;
+    padding: 5px 10px;
+    border: 0.5px solid #007bff;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -15,13 +15,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-    server: {
-        proxy: {
-            '/api': {
-                target: process.env.IP,
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-            },
-        },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/home'),
+      },
     },
+  },
 })
